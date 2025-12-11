6151294544815d56168c662353a2212f1cb668a4
UnzelaFareed02 <fa23-bse-189@cuilahore.edu.pk>
2025-12-11 19:35:29 +0500

    Remove root files

--- src/api/authApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api/auth';
-
-export const registerUser = (data) => axios.post(`${API_URL}/register`, data);
-export const loginUser = (data) => axios.post(`${API_URL}/login`, data);
